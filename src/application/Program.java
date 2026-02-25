5c828f5f30d176868ebf127fb81cf92fd218e8cb
Marcelo Parabocz <macerno@gmail.com>
2026-02-25 10:00:39 -0300

    feat: seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department department = new Department(1, "Books");
-		System.out.println(department);
+
+		Seller seller = new Seller(1, "Adam", "Adam@outlook.com", LocalDate.now(), 2500.0, department);
+		System.out.println(seller);
 
 	}
 
