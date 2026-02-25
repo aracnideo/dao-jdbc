dea5b1d03dc526d61aa58165ab29ac8b8f400db8
Marcelo Parabocz <macerno@gmail.com>
2026-02-25 10:15:09 -0300

    SellerDaoJDBC and DaoFactory class

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.time.LocalDate;
 
+import model.dao.DaoFactory;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,8 +10,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		Department department = new Department(1, "Books");
-
 		Seller seller = new Seller(1, "Adam", "Adam@outlook.com", LocalDate.now(), 2500.0, department);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 
 	}
